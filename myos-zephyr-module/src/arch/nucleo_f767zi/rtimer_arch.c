43b1cee2c62d4307c31b224484f4e4381d2dc8c2
Marco Bacchi <marco@bacchi.at>
2025-12-04 16:11:56 +0100

    feat: switch rtimer and timestamp from TIM6 to TIM9 on Nucleo‑F767ZI

--- myos-zephyr-module/src/arch/nucleo_f767zi/rtimer_arch.c
+++ myos-zephyr-module/src/arch/nucleo_f767zi/rtimer_arch.c
@@ -10,32 +10,46 @@
 #define DBG(...) do{}while(0)
 #endif
 
-/* Das ist der "counter" Subnode unter timers6 */
-#define TIM6_COUNTER_NODE DT_CHILD(DT_NODELABEL(timers6), counter)
-
-const struct device *const tim6_counter_dev =
-    DEVICE_DT_GET(TIM6_COUNTER_NODE);
+extern const struct device *const tim9_counter_dev;
 
 void rtimer_arch_init()
 {
-    if (!device_is_ready(tim6_counter_dev)) 
-	{
-    	DBG("rtimer: TIM6 counter device not ready!\n");
-		return;	
-	}
 
-    if (counter_start(tim6_counter_dev) != 0) 
-	{
-        DBG("rtimer: counter_start failed: \n");
-        return;
-    }    
 }
 
 rtimer_arch_timestamp_t rtimer_arch_now(void)
 {
     uint32_t timestamp;
-    counter_get_value(tim6_counter_dev, &timestamp);
+    counter_get_value(tim9_counter_dev, &timestamp);
     return (rtimer_arch_timestamp_t)timestamp;
+}
+
+
+extern void rtimer_scheduler (void);
+void rtimer_arch_timer_callback(const struct device *dev,
+                                uint8_t chan_id,
+                                uint32_t ticks,
+                                void *user_data)
+{
+    ARG_UNUSED(dev);
+    ARG_UNUSED(chan_id);
+    ARG_UNUSED(user_data);
+
+    rtimer_scheduler(); 
+}
+
+
+
+
+void rtimer_arch_timer_set(rtimer_arch_timestamp_t stop)
+{
+    struct counter_alarm_cfg alarm_cfg = {0};
+    alarm_cfg.flags = COUNTER_ALARM_CFG_ABSOLUTE;
+    alarm_cfg.ticks = stop;
+    alarm_cfg.callback = rtimer_arch_timer_callback;  
+    alarm_cfg.user_data = NULL;
+
+    counter_set_channel_alarm(tim9_counter_dev, 0, &alarm_cfg);
 }
 
 
--- myos-zephyr-module/src/arch/nucleo_f767zi/timestamp_arch.c
+++ myos-zephyr-module/src/arch/nucleo_f767zi/timestamp_arch.c
@@ -29,11 +29,105 @@
 
 #include"timestamp_arch.h" 
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/counter.h> 	
+#include "debug.h"
+#include "myos.h"
+
+#if defined(CONFIG_MYOS_DEBUG_TIMESTAMP)
+#define DBG(...) DBG_FUNC(__VA_ARGS__)
+#else
+#define DBG(...) do{}while(0)
+#endif
+
+static timestamp_arch_t timestamp_counter = 0;
+
+#define TIM21_COUNTER_NODE DT_CHILD(DT_NODELABEL(timers9), counter)
+
+const struct device *const tim9_counter_dev =
+    DEVICE_DT_GET(TIM21_COUNTER_NODE);
 
 timestamp_arch_t timestamp_arch_now(void)
 {
-      return (timestamp_arch_t)k_uptime_get_32(); 
+      timestamp_arch_t t1,t2;
+
+      do{
+            t1 = timestamp_counter;
+            t2 = timestamp_counter;
+      }while(t1 != t2);
+
+      return t1;
+}
+
+#define TIM21_TICKS_PER_MS 50U   /* 50 kHz -> 1 ms = 50 ticks */
+
+
+void timestamp_arch_myos_tick(const struct device *dev,
+                                uint8_t chan_id,
+                                uint32_t ticks,
+                                void *user_data);
+
+static struct counter_alarm_cfg alarm_cfg = {
+            .flags    = COUNTER_ALARM_CFG_ABSOLUTE,
+            .ticks    = 0,
+            .callback = timestamp_arch_myos_tick,
+            .user_data = NULL,
+      };
+
+void timestamp_arch_myos_tick(const struct device *dev,
+                                uint8_t chan_id,
+                                uint32_t ticks,
+                                void *user_data)
+{
+      ARG_UNUSED(user_data);
+
+      timestamp_counter++;
+
+      // Process ptimers if there are pending timers and the next stop time has passed
+      if (ptimer_pending && timestamp_passed(ptimer_next_stop)) 
+      {
+            ptimer_pending = false;
+            process_poll(&ptimer_process);
+      }
+
+
+       /* Re-arm same channel (1) for the next 1 ms */
+      alarm_cfg.ticks = (timestamp_arch_t)(ticks + TIM21_TICKS_PER_MS);
+     
+      if (counter_set_channel_alarm(dev, chan_id, &alarm_cfg) != 0)
+      {
+         DBG("timestamp: set channel alarm failed %ul!\n",alarm_cfg.ticks);  
+      }
+
 }
 
 
+void timestamp_arch_module_init(void)
+{
 
+      uint32_t now;
+      int err;
+      
+      if (!device_is_ready(tim9_counter_dev)) 
+	{
+    	DBG("timestamp: counter device not ready!\n");
+		return;	
+	}
+
+      if (counter_start(tim9_counter_dev) != 0) 
+            {
+            DBG("timestamp: counter_start failed: \n");
+            return;
+      }   
+
+      err = counter_get_value(tim9_counter_dev, &now);
+      if (err < 0) {
+            DBG("timestamp: counter_get_value failed: %d\n", err);
+            return;
+      }
+
+      alarm_cfg.ticks = (timestamp_arch_t)(now + TIM21_TICKS_PER_MS);
+
+      counter_set_channel_alarm(tim9_counter_dev, 1, &alarm_cfg);
+      
+      DBG("timetamp: initialized: \n");
+}
